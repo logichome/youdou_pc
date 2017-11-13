79b2caa9b75e07ae1a9c15951d0fbcf79068e51d
Rico <rico@logichome.me>
2017-11-13 14:45:45 +0800

    no message

--- src/router/job_manage.js
+++ src/router/job_manage.js
@@ -1,25 +1,24 @@
 import jobManage from '@/modules/job_manage/components/job_manage'
-import interview from '@/modules/job_manage/components/job_manage_interview'
-import offer from '@/modules/job_manage/components/job_manage_offer'
+
 
 
 export default [
   {
     path: 'job_manage',
     component: jobManage,
-    children:[
-      {
-        path:'',
-        redirect:'interview'
-      },
-      {
-        path:'interview',
-        component:interview
-      },
-      {
-        path:'offer',
-        component:offer
-      },
-    ]
+    // children:[
+    //   {
+    //     path:'',
+    //     redirect:'interview'
+    //   },
+    //   {
+    //     path:'interview',
+    //     component:interview
+    //   },
+    //   {
+    //     path:'offer',
+    //     component:offer
+    //   },
+    // ]
   }
 ]
--- src/router/work_manage.js
+++ src/router/work_manage.js
@@ -1,8 +1,24 @@
 import workManage from '@/modules/work_manage/components/work_manage'
+import interview from '@/modules/work_manage/components/work_manage_interview'
+import offer from '@/modules/work_manage/components/work_manage_offer'
 
 export default [
   {
     path: 'work_manage',
-    component: workManage
+    component: workManage,
+    children:[
+      {
+        path:'',
+        redirect:'interview'
+      },
+      {
+        path:'interview',
+        component:interview
+      },
+      {
+        path:'offer',
+        component:offer
+      },
+    ]
   }
 ]
